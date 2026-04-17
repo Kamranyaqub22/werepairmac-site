10a2885e31e7792a38a34be136c8779a3f544add
Kamranyaqub22 <127260114+Kamranyaqub22@users.noreply.github.com>
2026-04-17 10:04:21 +0100

    feat: expanded geographical coverage mapping with 28 new service locations

--- lib/locations.ts
+++ lib/locations.ts
@@ -45,6 +45,44 @@ export const locations: Location[] = [
   { slug: 'lewisham', name: 'Lewisham', postcode: 'SE13', borough: 'Lewisham', description: 'Lewisham town centre' },
   { slug: 'catford', name: 'Catford', postcode: 'SE6', borough: 'Lewisham', description: 'Catford and Hither Green' },
   { slug: 'greenwich', name: 'Greenwich', postcode: 'SE10', borough: 'Greenwich', description: 'Greenwich and Blackheath' },
+  
+  // North London
+  { slug: 'islington', name: 'Islington', postcode: 'N1', borough: 'Islington', description: 'Islington and Angel area' },
+  { slug: 'camden', name: 'Camden', postcode: 'NW1', borough: 'Camden', description: 'Camden Town and Chalk Farm' },
+  { slug: 'finchley', name: 'Finchley', postcode: 'N3', borough: 'Barnet', description: 'Finchley and Woodhouse' },
+  { slug: 'barnet', name: 'Barnet', postcode: 'EN5', borough: 'Barnet', description: 'High Barnet and Chipping Barnet' },
+  { slug: 'enfield', name: 'Enfield', postcode: 'EN1', borough: 'Enfield', description: 'Enfield Town and surrounding areas' },
+  { slug: 'haringey', name: 'Haringey', postcode: 'N8', borough: 'Haringey', description: 'Haringey and Hornsey' },
+  
+  // East London
+  { slug: 'stratford', name: 'Stratford', postcode: 'E15', borough: 'Newham', description: 'Stratford and Queen Elizabeth Olympic Park' },
+  { slug: 'canary-wharf', name: 'Canary Wharf', postcode: 'E14', borough: 'Tower Hamlets', description: 'Canary Wharf and Isle of Dogs' },
+  { slug: 'hackney', name: 'Hackney', postcode: 'E8', borough: 'Hackney', description: 'Hackney Central and Dalston' },
+  { slug: 'shoreditch', name: 'Shoreditch', postcode: 'E1', borough: 'Hackney', description: 'Shoreditch and Hoxton' },
+  { slug: 'ilford', name: 'Ilford', postcode: 'IG1', borough: 'Redbridge', description: 'Ilford and Seven Kings' },
+  { slug: 'romford', name: 'Romford', postcode: 'RM1', borough: 'Havering', description: 'Romford town centre' },
+
+  // Further West London & Middlesex
+  { slug: 'chiswick', name: 'Chiswick', postcode: 'W4', borough: 'Hounslow', description: 'Chiswick and Turnham Green' },
+  { slug: 'ealing', name: 'Ealing', postcode: 'W5', borough: 'Ealing', description: 'Ealing Broadway and West Ealing' },
+  { slug: 'hounslow', name: 'Hounslow', postcode: 'TW3', borough: 'Hounslow', description: 'Hounslow Central and West' },
+  { slug: 'uxbridge', name: 'Uxbridge', postcode: 'UB8', borough: 'Hillingdon', description: 'Uxbridge town centre' },
+  { slug: 'hayes', name: 'Hayes', postcode: 'UB3', borough: 'Hillingdon', description: 'Hayes and Harlington' },
+
+  // South East London Additions
+  { slug: 'dulwich', name: 'Dulwich', postcode: 'SE21', borough: 'Southwark', description: 'Dulwich and West Dulwich' },
+  { slug: 'orpington', name: 'Orpington', postcode: 'BR6', borough: 'Bromley', description: 'Orpington and Petts Wood' },
+  { slug: 'peckham', name: 'Peckham', postcode: 'SE15', borough: 'Southwark', description: 'Peckham and Nunhead' },
+  { slug: 'dartford', name: 'Dartford', postcode: 'DA1', borough: 'Dartford', description: 'Dartford and surrounding areas' },
+
+  // More Surrey / Greater London Border Additions
+  { slug: 'weybridge', name: 'Weybridge', postcode: 'KT13', borough: 'Elmbridge', description: 'Weybridge and Oatlands' },
+  { slug: 'walton-on-thames', name: 'Walton-on-Thames', postcode: 'KT12', borough: 'Elmbridge', description: 'Walton-on-Thames and Hersham' },
+  { slug: 'leatherhead', name: 'Leatherhead', postcode: 'KT22', borough: 'Mole Valley', description: 'Leatherhead and Fetcham' },
+  { slug: 'cobham', name: 'Cobham', postcode: 'KT11', borough: 'Elmbridge', description: 'Cobham and Stoke d\'Abernon' },
+  { slug: 'dorking', name: 'Dorking', postcode: 'RH4', borough: 'Mole Valley', description: 'Dorking and surrounding villages' },
+  { slug: 'woking', name: 'Woking', postcode: 'GU21', borough: 'Woking', description: 'Woking town centre' },
+  { slug: 'guildford', name: 'Guildford', postcode: 'GU1', borough: 'Guildford', description: 'Guildford and surrounding areas' }
 ];
 
 export function getLocation(slug: string): Location | undefined {
